853a06dd1ca20554eb9cb3db3a516c9c720f3d3d
Elias Lima <sci.eliaslima@gmail.com>
2023-05-01 19:48:40 -0300

    feat(routes): add new screens to routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,8 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Screens
+import Onboarding from '@pages/Onboarding';
+import Register from '@pages/Register';
 import SplashScreen from '@pages/SplashScreen';
 
 const AuthRoutes = () => {
@@ -9,7 +11,9 @@ const AuthRoutes = () => {
 
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Splash" component={SplashScreen} />
+            <Stack.Screen name="Splash" component={SplashScreen}/>
+            <Stack.Screen name="Onboarding" component={Onboarding} options={{gestureEnabled: false}}/>
+            <Stack.Screen name="Register" component={Register} options={{presentation: 'modal'}}/>
         </Stack.Navigator>
     );
 }
