8a5d07c5cee60bb668482b6b9d9e117a8a5228e6
Geovana <geovanaf1912@gmail.com>
2023-05-01 11:28:13 -0300

    feat: added endpoints

---
+++ src/services/api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+//TODO: conferir se é essa porta mesmo
+const baseUrl = "http://localhost:8000";
+
+const api = axios.create({
+  baseUrl,
+});
+
+//TODO: ver se o back end não permite fazer pesquisa
+export async function getUserList(data = {}) {
+  const response = await api.get(`userList/${data && data}`);
+
+  return response.data;
+}
+
+// TODO: nao achei rota para getUserById
+export async function getUserById(id) {
+  const response = await api.get(`/userList/${id}`);
+
+  return response.data;
+}
+
+export async function createUser(data) {
+  const response = await api.post(`createUser/${data}`);
+
+  return response.data;
+}
+
+export async function deleteUser(id) {
+  const response = await api.delete(`deleteUser/${id}`);
+
+  return response.data;
+}
