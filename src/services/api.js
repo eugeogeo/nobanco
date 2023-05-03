571946dae63f115f4c513eac00dff1631cbe069a
Geovana Figueiredo Silva <geovanaf1912@gmail.com>
2023-05-03 19:32:28 -0300

    feat: configurado api e base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,28 @@
 import axios from "axios";
 
 //TODO: conferir se é essa porta mesmo
+// const baseUrl = "http://192.168.1.9:8000";
 const baseUrl = "http://localhost:8000";
 
 const api = axios.create({
   baseUrl,
 });
 
+export async function createUser(data) {
+  const response = await api.post("/createUser/", {...data, cellPhone: "(44) 99999-9999" });
+
+  return response;
+}
+
+export async function deleteUser(id) {
+  const response = await api.delete(`deleteUser/${id}`);
+
+  return response.data;
+}
+
 //TODO: ver se o back end não permite fazer pesquisa
 export async function getUserList(data = {}) {
-  const response = await api.get(`userList/${data && data}`);
+  const response = await api.get(`/userList/${data && data}`);
 
   return response.data;
 }
@@ -17,18 +30,6 @@ export async function getUserList(data = {}) {
 // TODO: nao achei rota para getUserById
 export async function getUserById(id) {
   const response = await api.get(`/userList/${id}`);
-
-  return response.data;
-}
-
-export async function createUser(data) {
-  const response = await api.post(`createUser/${data}`);
-
-  return response.data;
-}
-
-export async function deleteUser(id) {
-  const response = await api.delete(`deleteUser/${id}`);
 
   return response.data;
 }
